54ad94b6a53812622fbda8f62f42fec3793e0d16
Asliddin <Eshkhuvvatofff@gmail.com>
2024-12-24 20:23:23 +0500

    404 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import AddGame from './Pages/AddGames/AddGames';
 import LandingPage from './Pages/Landing/LandingPage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import NotFound from './Pages/NotFound/NotFound';
+import './App.css';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
+      <AppContent />
+    </Router>
+  );
+};
+
+const AppContent: React.FC = () => {
+  const location = useLocation();
+
+  const shouldShowHeaderFooter = location.pathname !== '/404'; 
+
+  return (
+    <>
+      {shouldShowHeaderFooter && <Header />}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/add-game" element={<AddGame />} />
+        <Route path="/*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<NotFound />} />
       </Routes>
-      <Footer />
-    </Router>
+      {shouldShowHeaderFooter && <Footer />}
+    </>
   );
 };
 
