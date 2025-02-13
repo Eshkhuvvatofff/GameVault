687d63826f5690eba39bc9166e49a6e02e6209e6
Asliddin <Eshkhuvvatofff@gmail.com>
2025-02-14 00:02:16 +0500

    remove backgroundslider

--- src/Pages/Landing/LandingPage.tsx
+++ src/Pages/Landing/LandingPage.tsx
@@ -1,16 +1,11 @@
-import BackgroundSlider from "@/components/homepage(assets)/background/BackgroundSlider";
+import ParticleBackground from "@/components/ParticleBackground/ParticleBackground"
 
 const LandingPage = () => {
-  const slides = [
-    "https://gamevault-assets.vercel.app/backgrounds/landing-page/video/wall.mp4",
-    "https://static.vecteezy.com/system/resources/previews/049/855/259/non_2x/nature-background-high-resolution-wallpaper-for-a-serene-and-stunning-view-photo.jpg",
-    "https://t3.ftcdn.net/jpg/08/06/10/36/360_F_806103697_E9Y1vKhtQimCEIiA75QWEn4NdZe7lQXj.jpg",
-  ];
+  
   return (
     <div>
-      {/* <BackgroundSlider slides={slides} interval={9000} /> */}
-      <BackgroundSlider slides={slides} interval={7000} duration={1.5} />
       <div className="">
+        <ParticleBackground />
         <h1>content here</h1>
       </div>
     </div>
